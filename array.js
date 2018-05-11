9a04e40fe3911a6bf65fca5fc827b51aa3639471
Programming <programming@us-physics-cart-15.local>
2018-05-11 13:59:47 -0400

    loops day 1

--- array.js
+++ array.js
@@ -3,7 +3,7 @@
 function min(inArray) {
     // We do this by stepping through each entry of the array.
     // First, set the minimum to be the first value in the array.
-    let min = inArray[/*insert the appropriate value here*/];
+    let min = inArray[0];
 
     // Now go through all the remaining entries of the array
     // and determine if the current entry should become the new minimum
@@ -15,16 +15,27 @@ function min(inArray) {
     }
 
     // Now that the for loop has finished, log the minimum to the console.
-    console.log(/* Put the appropriate variable here */);
+    console.log("The minimum value is " + min + ".");
 }
 
 // The function max will take in an array and log its maximum value.
 function max(inArray) {
+    let max = inArray[0];
+    for (let i = 1; i < inArray.length; i += 1) {
+        // If the current value is less than the minimum then it becomes the new minimum
+        if (inArray[i] > max) {
+            max = inArray[i];
+        }
+    }
 
+    // Now that the for loop has finished, log the minimum to the console.
+    console.log("The maximum value is " + max + ".");
 }
 
 // The function sum will take in an array and log the sum of the array.
 function sum(inArray) {
+
+    console.log("The soum of the array is  " + sum+ ".");
 
 }
 
